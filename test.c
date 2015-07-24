bacd9d4b63a11428c16a3b6d907031b25a95a2ac
ALEXANDER LUBYAGIN <lubyagin@yandex.ru>
2015-07-24 14:15:01 +0300

    test.c fix

--- test.c
+++ test.c
@@ -1,16 +1,34 @@
 #include "sbt.h"
+#include <stdio.h>
 
 int main() {
 
 	SBT_Initialise();
-	int i;
-	for(i = 0; i < 1000000; i++) {
-            SBT_AddNode(i%100000);
-	}
 
-//        SBT_PrintAllNodes();
-//        SBT_DumpAllNodes();
+	SBT_AddNode(0);
+	SBT_AddNode(1);
+	SBT_AddNode(2);
+	SBT_AddNode(3);
+	SBT_AddNode(4);
+	SBT_AddNode(5);
+	SBT_AddNode(6);
+	SBT_AddNode(7);
+	SBT_AddNode(8);
+	SBT_AddNode(9);
+	SBT_AddNode(10);
+	SBT_AddNode(11);
+	SBT_AddNode(123);
+	SBT_PrintAllNodes();
+	SBT_DumpAllNodes();
+	for (int k = 0; k < 20; k++) SBT_AddNode(k);
+
+	printf("Value %2I64d\n",(long long int)aValue[0]);
+	printf("UT    %2I64d\n",(long long int)aUT[0]);
+	printf("LT    %2I64d\n",(long long int)aLT[0]);
+	printf("RT    %2I64d\n",(long long int)aRT[0]);
+	printf("Size  %2I64d\n",(long long int)aSize[0]);
+	printf("FREE  %2I64d\n",(long long int)aFREE[0]);
 
 	SBT_Deinitialise();
-        return 0;
+	return 0;
 }

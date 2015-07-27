e53b7c989ea219991cc37b981f6f04336b52b975
ALEXANDER LUBYAGIN <lubyagin@yandex.ru>
2015-07-27 11:13:59 +0300

    SBT_LeftRotate_Opt, SBT_RightRotate_Opt()

--- func.asm
+++ func.asm
@@ -2,8 +2,11 @@
 	SECTION .text
 
 	global SBT_Initialise_Opt
+	global SBT_LeftRotate_Opt
+	global SBT_RightRotate_Opt
 
 	extern NN
+	extern ROOT
 	extern aValue
 	extern aUT
 	extern aLT
@@ -76,4 +79,206 @@ SBT_Initialise_Opt:
 	ADD RBX,1 ; +BYTE uint8_t
 	LOOPNZ @LoopFREE
 
+	RET
+
+; цель оптимизации SBTAMD64 в том, чтобы
+; достичь скорости вставки 10 млн. вершин за 1 сек,
+; т.е. 300 тактов на 3-ГГц ядре процессора на вставку
+
+; -------------------------------------------------------
+SBT_LeftRotate_Opt:
+	; TNodeIndex t
+	CMP RCX,0
+	JB @SBT_LeftRotateRet0 ; if (t < 0) return 0;
+	MOV R8,[aRT]
+	MOV RAX,qword [R8+RCX*8] ; TNodeIndex k = aRT[t];
+	CMP RAX,0
+	JB @SBT_LeftRotateRet0 ; if (k < 0) return 0;
+	MOV R9,[aUT]
+	MOV RBX,qword [R9+RCX*8] ; TNodeIndex p = aUT[t];
+	; RAX = k;
+	; RBX = p;
+	; RCX = t;
+
+	; RAX,RBX,RCX
+	; R8 = [aRT]
+	; R9 = [aUT]
+	; R10 = [aLT]
+
+	; поворачиваем ребро дерева
+	MOV R10,[aLT]
+	MOV RDX,qword [R10+RAX*8]
+	MOV qword [R8+RCX*8],RDX ; aRT[t] = aLT[k];
+	MOV qword [R10+RAX*8],RCX ; aLT[k] = t;
+
+	; корректируем size
+	; R11 = [aSize]
+	MOV R11,[aSize]
+	MOV RDX,qword [R11+RCX*8] ; ? меньше использовать один регистр
+	MOV qword [R11+RAX*8],RDX ; aSize[k] = aSize[t];
+
+	; R12 = n_l
+	; R13 = n_r
+	; R14 = s_l
+	; R15 = s_r -> SUM
+	MOV R12,qword [R10+RCX*8] ; TNodeIndex n_l = aLT[t];
+	MOV R13,qword [R8+RCX*8] ; TNodeIndex n_r = aRT[t];
+
+	CMP R12,-1
+	JNE @SBT_LeftRotateSLN ; (n_l != -1)
+	MOV R14,0
+	JMP @SBT_LeftRotateSLN0
+@SBT_LeftRotateSLN:
+	MOV R14,qword [R11+R12*8] ; TNodeSize s_l = ((n_l != -1) ? aSize[n_l] : 0);
+@SBT_LeftRotateSLN0:
+
+	CMP R13,-1
+	JNE @SBT_LeftRotateSRN ; (n_r != -1)
+	MOV R15,0
+	JMP @SBT_LeftRotateSRN0
+@SBT_LeftRotateSRN:
+	MOV R15,qword [R11+R13*8] ; TNodeSize s_r = ((n_r != -1) ? aSize[n_r] : 0);
+@SBT_LeftRotateSRN0:
+
+	ADD R15,R14 ; aSize[t] = s_l + s_r + 1;
+	INC R15
+	MOV qword [R11+RCX*8],RDX
+
+	; меняем трёх предков
+	; 1. t.right.parent = t
+	; 2. k.parent = t.parent
+	; 3. t.parent = k
+
+	MOV RDX,qword [R8+RCX*8] ; aRT[t]
+	CMP RDX,-1
+	JE @SBT_LeftRotateRT1
+	MOV qword [R9+RDX*8],RCX ; if (aRT[t] != -1) aUT[aRT[t]] = t;
+@SBT_LeftRotateRT1:
+	MOV qword [R9+RAX*8],RBX ; aUT[k] = p;
+	MOV qword [R9+RCX*8],RAX ; aUT[t] = k;
+
+	; меняем корень, parent -> t, k
+
+	CMP RBX,-1
+	JE @SBT_LeftRotateRBX0
+
+	MOV RDX,qword [R10+RBX*8] ; aLT[p]
+	CMP RDX,RCX
+	JNE @SBT_LeftRotateRTPK
+
+	MOV qword [R10+RBX*8],RAX ; if (aLT[p] == t) aLT[p] = k;
+	MOV RAX,1
+	RET
+@SBT_LeftRotateRTPK:
+	MOV qword [R8+RBX*8],RAX ; else aRT[p] = k;
+	MOV RAX,1
+	RET
+
+@SBT_LeftRotateRBX0:
+	MOV RDX,[ROOT]
+	MOV qword [RDX],RAX ; if (p == -1) ROOT = k;
+	MOV RAX,1
+	RET
+
+@SBT_LeftRotateRet0:
+	MOV RAX,0
+	RET
+
+; -------------------------------------------------------
+SBT_RightRotate_Opt:
+	; TNodeIndex t
+	CMP RCX,0
+	JB @SBT_RightRotateRet0 ; if (t < 0) return 0;
+	MOV R8,[aLT]
+	MOV RAX,qword [R8+RCX*8] ; TNodeIndex k = aLT[t];
+	CMP RAX,0
+	JB @SBT_RightRotateRet0 ; if (k < 0) return 0;
+	MOV R9,[aUT]
+	MOV RBX,qword [R9+RCX*8] ; TNodeIndex p = aUT[t];
+	; RAX = k;
+	; RBX = p;
+	; RCX = t;
+
+	; RAX,RBX,RCX
+	; R8 = [aRT]  ; [aLT]
+	; R9 = [aUT]
+	; R10 = [aLT] ; [aRT]
+
+	; поворачиваем ребро дерева
+	MOV R10,[aRT]
+	MOV RDX,qword [R10+RAX*8]
+	MOV qword [R8+RCX*8],RDX ; aLT[t] = aRT[k];
+	MOV qword [R10+RAX*8],RCX ; aRT[k] = t;
+
+	; корректируем size
+	; R11 = [aSize]
+	MOV R11,[aSize]
+	MOV RDX,qword [R11+RCX*8] ; ? меньше использовать один регистр
+	MOV qword [R11+RAX*8],RDX ; aSize[k] = aSize[t];
+
+	; R12 = n_l
+	; R13 = n_r
+	; R14 = s_l
+	; R15 = s_r -> SUM
+	MOV R12,qword [R8+RCX*8] ; TNodeIndex n_l = aLT[t];
+	MOV R13,qword [R10+RCX*8] ; TNodeIndex n_r = aRT[t];
+
+	CMP R12,-1
+	JNE @SBT_RightRotateSLN ; (n_l != -1)
+	MOV R14,0
+	JMP @SBT_RightRotateSLN0
+@SBT_RightRotateSLN:
+	MOV R14,qword [R11+R12*8] ; TNodeSize s_l = ((n_l != -1) ? aSize[n_l] : 0);
+@SBT_RightRotateSLN0:
+
+	CMP R13,-1
+	JNE @SBT_RightRotateSRN ; (n_r != -1)
+	MOV R15,0
+	JMP @SBT_RightRotateSRN0
+@SBT_RightRotateSRN:
+	MOV R15,qword [R11+R13*8] ; TNodeSize s_r = ((n_r != -1) ? aSize[n_r] : 0);
+@SBT_RightRotateSRN0:
+
+	ADD R15,R14 ; aSize[t] = s_l + s_r + 1;
+	INC R15
+	MOV qword [R11+RCX*8],RDX
+
+	; меняем трёх предков
+	; 1. t.right.parent = t
+	; 2. k.parent = t.parent
+	; 3. t.parent = k
+
+	MOV RDX,qword [R8+RCX*8] ; aRT[t] ; aLT[t]
+	CMP RDX,-1
+	JE @SBT_RightRotateRT1
+	MOV qword [R9+RDX*8],RCX ; if (aLT[t] != -1) aUT[aLT[t]] = t;
+@SBT_RightRotateRT1:
+	MOV qword [R9+RAX*8],RBX ; aUT[k] = p;
+	MOV qword [R9+RCX*8],RAX ; aUT[t] = k;
+
+	; меняем корень, parent -> t, k
+
+	CMP RBX,-1
+	JE @SBT_RightRotateRBX0
+
+	MOV RDX,qword [R8+RBX*8] ; aLT[p]
+	CMP RDX,RCX
+	JNE @SBT_RightRotateRTPK
+
+	MOV qword [R8+RBX*8],RAX ; if (aLT[p] == t) aLT[p] = k;
+	MOV RAX,1
+	RET
+@SBT_RightRotateRTPK:
+	MOV qword [R10+RBX*8],RAX ; else aRT[p] = k;
+	MOV RAX,1
+	RET
+
+@SBT_RightRotateRBX0:
+	MOV RDX,[ROOT]
+	MOV qword [RDX],RAX ; if (p == -1) ROOT = k;
+	MOV RAX,1
+	RET
+
+@SBT_RightRotateRet0:
+	MOV RAX,0
 	RET

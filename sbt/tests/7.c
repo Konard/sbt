7209d24694f8459e69b55b09a6f1b42a83f7dd74
ALEXANDER LUBYAGIN <lubyagin@yandex.ru>
2015-08-19 14:13:10 +0300

    small fix

--- sbt/tests/7.c
+++ sbt/tests/7.c
@@ -10,24 +10,17 @@
 
 int main(int argc, char **argv) {
 
-	if (argc < 2) return 0;
-	int N = atoi(argv[1]);
+	int N = 10000000;
 
 	printf("добавление ...\n");
 	for (int i = 0; i < N; i++) {
 		SBT_AddNode(i);
-//		SBT_AddNodeUniq(i);
-//		SBT_CheckAllNodesSize();
 	}
 
 	printf("удаление ...\n");
 	for (int i = 0; i < N; i++) {
 		SBT_DeleteNode(i);
-//		SBT_CheckAllNodesBalance();
 	}
-
-//	SBT_CheckAllNodes();
-//	SBT_PrintAllNodes();
 
 	return 0;
 }
